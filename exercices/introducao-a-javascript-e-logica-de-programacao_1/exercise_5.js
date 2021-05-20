24a97b500f65c8c9f6b3782b0db1a0c16148a808
Ramon Vitor <vitorramon@hotmail.com>
2021-05-20 11:45:39 -0300

    Correção do exercício 5

--- exercices/introducao-a-javascript-e-logica-de-programacao_1/exercise_5.js
+++ exercices/introducao-a-javascript-e-logica-de-programacao_1/exercise_5.js
@@ -5,6 +5,9 @@ let angulo3 = 60;
 if (angulo1 == angulo2 && angulo1 == angulo3) {
     console.log("True")
 }
+else if (angulo1 < 0 || angulo2 < 0 || angulo3 < 0) {
+    console.log("Valor inválido, o ângulo não pode ser um valor negativo.")
+}
 else {
     console.log("False")
 }
