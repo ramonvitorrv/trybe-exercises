06bf8ad4b16e881ec61036ad04b4da5e62ced2ec
Ramon Vitor <vitorramon@hotmail.com>
2021-05-23 17:13:07 -0300

    Resolvido exercício 1

---
+++ exercices/introducao-a-javascript-e-logica-de-programacao_3/exercise_1.js
@@ -0,0 +1,11 @@
+let numero = 5;
+let asterisco = '*';
+let stringVazia = '';
+
+for (let index = 0; index < numero; index += 1) {
+    stringVazia += asterisco;
+}
+
+for (let index = 0; index < numero; index += 1) {
+    console.log(stringVazia);
+}
