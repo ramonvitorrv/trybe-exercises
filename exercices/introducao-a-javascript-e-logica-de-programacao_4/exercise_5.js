3dd43d626c3d93fe0ddf73f683ceb77bed430f3d
Ramon Vitor <vitorramon@hotmail.com>
2021-05-25 12:58:07 -0300

    Resolvido exercício 5

---
+++ exercices/introducao-a-javascript-e-logica-de-programacao_4/exercise_5.js
@@ -0,0 +1,23 @@
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: 'Sim'
+  };
+
+let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim'
+  };
+
+for (let keys in info) {
+    if (
+      info[keys] === info.recorrente && info[keys] === 'Sim' && info2[keys] === 'Sim'
+    ) {
+      console.log('Ambos recorrentes');
+    } else {
+      console.log(`${info[keys]} e ${info2[keys]}`);
+    }
+  }
