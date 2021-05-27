7d205c8721d3cad8a473fc4d3f8627f9fe8e8060
Ramon Vitor <vitorramon@hotmail.com>
2021-05-27 10:37:30 -0300

    Resolvido exercício da parte 3

---
+++ exercices/Bloco 5/5.1/script.js
@@ -0,0 +1,22 @@
+// Tag header:
+document.querySelector("#header-container").style.backgroundColor = "green";
+
+// Tag body:
+document.querySelector("#container").style.backgroundColor = "grey";
+
+// Tag Section 1:
+document.querySelector(".emergency-tasks").style.backgroundColor = "orange";
+
+// Tag h3 na Section 1:
+document.querySelectorAll(".emergency-tasks h3")[0].style.backgroundColor = "purple";
+document.querySelectorAll(".emergency-tasks h3")[1].style.backgroundColor = "purple";
+
+// Tag Section 2:
+document.querySelector(".no-emergency-tasks").style.backgroundColor = "yellow";
+
+//Tag h3 na Section 2:
+document.querySelectorAll(".no-emergency-tasks h3")[0].style.backgroundColor = "black";
+document.querySelectorAll(".no-emergency-tasks h3")[1].style.backgroundColor = "black";
+
+// Tag footer:
+document.querySelector("#footer-container").style.backgroundColor = "black";
