604592cb3fcf814ea22ef6e7c14fc747265f625a
Ramon Vitor <vitorramon@hotmail.com>
2021-05-30 18:01:01 -0300

    Corrigido comentario no exercicio bonus-2 e corrigido o exercício bonus-

--- exercices/introducao-a-javascript-e-logica-de-programacao_2/bônus_2.js
+++ exercices/introducao-a-javascript-e-logica-de-programacao_2/bônus_2.js
@@ -4,7 +4,7 @@ let array = [45, 27, 57, 1330, 560, 800, 200, 70, 945, 54]
 for (let i = 1; i < array.length; i += 1) {
 // Verifica a ordem dos números:
   for (let j = 0; j < array.length; j += 1)
-// Se o Valor da posição um do array for maior que o da posição 2:
+// Se o Valor da posição um do array for menor que o da posição 2:
   if (array[j] < array[j + 1]) {
 // Inverte a posição:
     let inverteNum = array[j];
--- exercices/introducao-a-javascript-e-logica-de-programacao_2/bônus_3.js
+++ exercices/introducao-a-javascript-e-logica-de-programacao_2/bônus_3.js
@@ -1,10 +1,14 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let array = [];
 
+// Percorre o array:
 for (let index = 0; index < numbers.length; index += 1) {
+  // Verifica se o index é menor que o tamanho do array:
       if (index + 1 < numbers.length) {
+        // Se for, "empurra" o valor dda posição 1 multiplicado pelo valor da posição 2:
         array.push(numbers[index] * numbers[index + 1])
       } else {
+        // Senão, multiplica por 2:
         array.push(numbers[index] * 2);
       }
   }
