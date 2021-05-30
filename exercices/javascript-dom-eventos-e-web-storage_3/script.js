d9a55d9aab7c3da03fbdd0c0f88f38ec429085e0
Ramon Vitor <vitorramon@hotmail.com>
2021-05-30 14:30:16 -0300

    Resolvido questão 1

---
+++ exercices/javascript-dom-eventos-e-web-storage_3/script.js
@@ -0,0 +1,35 @@
+function createDaysOfTheWeek() {
+  const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+  const weekDaysList = document.querySelector('.week-days');
+
+  for (let index = 0; index < weekDays.length; index += 1) {
+    const days = weekDays[index];
+    const dayListItem = document.createElement('li');
+    dayListItem.innerHTML = days;
+
+    weekDaysList.appendChild(dayListItem);
+  };
+};
+
+function createNumberOfDays() {
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  const days = document.querySelector('#days');
+
+  for (let index = 0 ; index < dezDaysList.length; index += 1) {
+    const day = dezDaysList[index]
+    const dayItem = document.createElement('li');
+    dayItem.className = "day"
+    if (day === 24 || day === 25 || day === 31){
+      dayItem.className = `${dayItem.getAttribute('class')} holiday`;
+    }
+    if (day === 4 || day === 11 || day === 18 || day === 25) {
+      dayItem.className = `${dayItem.getAttribute('class')} friday`;
+    }
+    dayItem.innerHTML = day;
+    days.appendChild(dayItem);
+  }
+
+}
+
+createDaysOfTheWeek();
+createNumberOfDays();
